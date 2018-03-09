2cef107becfda2b00f7edb5ad5acd879a387febc
robjordan <github@jordan-maynard.com>
2018-03-09 18:26:53 +0000

    Partially address #7 - not fully working

--- main/gps.c
+++ main/gps.c
@@ -15,7 +15,8 @@
 #define NAV_PVT_ID (0x01+(0x07<<8))
 #define NAV_PVT_VALID_DATE 0b001
 #define NAV_PVT_VALID_TIME 0b010
-#define GPS_BLINK_LED GPIO_NUM_27
+#define GPS_BLINK_RED GPIO_NUM_32
+#define GPS_BLINK_GREEN GPIO_NUM_33
 
 static QueueHandle_t uart0_queue;
 extern const char *TAG;
@@ -63,11 +64,11 @@ typedef struct {
 } gps_message;
 
 
-void gps_blink(unsigned ontime, unsigned offtime, unsigned repeats) {
+void gps_blink(unsigned led, unsigned ontime, unsigned offtime, unsigned repeats) {
     while (repeats > 0) {
-	gpio_set_level(GPS_BLINK_LED, 1);
+	gpio_set_level(led, 1);
 	vTaskDelay(ontime / portTICK_PERIOD_MS);
-	gpio_set_level(GPS_BLINK_LED, 0);
+	gpio_set_level(led, 0);
 	vTaskDelay(offtime / portTICK_PERIOD_MS);
 	repeats--;
     }
@@ -146,83 +147,172 @@ static void gps_write_gps_msg_to_file(gps_message *msg) {
     
 }
 
+void gps_init() {
+    // const char UBLOX_BAUD_RATE[] = {
+    // 0xB5,0x62,0x06,0x00,0x14,0x00,0x01,0x00,0x00,0x00,0x10,0x00,0x00,0x00,0x00,0xC2,0x01,0x00,0x01,0x00,0x01,0x00,0x00,
+    // 0x00,0x00,0x00,0xF0,0xCA // 115200 baud
+    // };
+    const char UBLOX_INIT[] = {
+	// Disable NMEA
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x24, // GxGGA off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x2B, // GxGLL off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x32, // GxGSA off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x39, // GxGSV off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x04,0x00,0x00,0x00,0x00,0x00,0x01,0x04,0x40, // GxRMC off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x05,0x00,0x00,0x00,0x00,0x00,0x01,0x05,0x47, // GxVTG off
 
-static void gps_handle_message(uint8_t *buf, size_t len) {
-    /* let's make the simplifying assumption that UBX messages won't span multiple UART events. */
-    /* it may not always be true, but empirically it seems to be the case. */
+	// Disable UBX
+	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x07,0x00,0x00,0x00,0x00,0x00,0x00,0x17,0xDC, //NAV-PVT off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x12,0xB9, //NAV-POSLLH off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x13,0xC0, //NAV-STATUS off
+	0xB5,0x62,0x06,0x01,0x08,0x00,0x05,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x15,0xD2, //ACK-ACK off
+
+	// Enable UBX
+	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x07,0x00,0x01,0x00,0x00,0x00,0x00,0x18,0xE1, //NAV-PVT on
+	// 0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x01,0x00,0x00,0x00,0x00,0x13,0xBE, //NAV-POSLLH on
+	// 0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x14,0xC5, //NAV-STATUS on
+
+	// Rate
+	// 0xB5,0x62,0x06,0x08,0x06,0x00,0x64,0x00,0x01,0x00,0x01,0x00,0x7A,0x12, //(10Hz)
+	// 0xB5,0x62,0x06,0x08,0x06,0x00,0xC8,0x00,0x01,0x00,0x01,0x00,0xDE,0x6A, //(5Hz)
+	// 0xB5,0x62,0x06,0x08,0x06,0x00,0xE8,0x03,0x01,0x00,0x01,0x00,0x01,0x39, //(1Hz)
+	0xB5,0x62,0x06,0x08,0x06,0x00,0x88,0x13,0x01,0x00,0x01,0x00,0xB1,0x49, //(0.2hz)
+
+	// GNSS type configuration
+	0xB5,0x62,0x06,0x3E,0x24,0x00,0x00,0x00,0x16,0x04,0x00,0x04,0xFF,0x00,0x01,0x00,0x00,0x01,0x01,0x01,0x03,0x00,0x01,
+	0x00,0x00,0x01,0x05,0x00,0x03,0x00,0x00,0x00,0x00,0x01,0x06,0x08,0xFF,0x00,0x00,0x00,0x00,0x01,0xA5,0x39,
+
+	// Power Save mode
+	0xB5,0x62,0x06,0x11,0x02,0x00,0x08,0x01,0x22,0x92
+    };
+
+    // uart_write_bytes(UART_NUM, UBLOX_BAUD_RATE, sizeof UBLOX_BAUD_RATE);
+    // ESP_ERROR_CHECK(uart_flush(UART_NUM));
+    // vTaskDelay(1000 / portTICK_RATE_MS); /* allow time for acks to come in */
+    // ESP_ERROR_CHECK(uart_set_baudrate(UART_NUM, 115200));
+    uint32_t baud_rate;
+    uart_get_baudrate(UART_NUM, &baud_rate);
+    ESP_LOGI(TAG, "baud rate: %d", baud_rate);
+    uart_write_bytes(UART_NUM, UBLOX_INIT, sizeof UBLOX_INIT);
+    return;
+}
+
+static void gps_handle_ubx_message(uint8_t *buf, size_t len) {
     static bool fix = false;
-    
-    /* search for 0xb5 0x62 as the signature for the start of a UBX message */
     uint8_t *p = buf;
-    while ((p != NULL) && (p < (buf + len))) {
-	if ((p = memchr(p, 0xb5, (buf + len) - p)) != NULL) {
-	    if (*(p+1) == 0x62) {
-		/* we've found the start of a UBX message */
-		gps_message *msg = (gps_message *)(p+2);
-		if (msg->id != NAV_PVT_ID) {
-		    /* it's not a message we can handle, log, ignore, and skip it */
-		    ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x not handled.\n", msg->id>>8, msg->id&0xFF);
+
+    ESP_LOGI(TAG, "gps_handle_ubx_message: %d", len);
+    ESP_LOG_BUFFER_HEXDUMP(TAG, buf, len, ESP_LOG_INFO);
+
+    
+    /* we've found the start of a UBX message */
+    gps_message *msg = (gps_message *)(p+2);
+    if (msg->id != NAV_PVT_ID) {
+	/* it's not a message we can handle, log, ignore, and skip it */
+	ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x not handled.\n", msg->id>>8, msg->id&0xFF);
 		    
-		} else if ((p + 2 + 2 + 2) > (buf + len)) {
-		    /* it's too short to even contain a payload length. skip it */
-		    ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x too short (no payload length).\n", msg->id>>8, msg->id&0xFF);
+    } else if ((p + 2 + 2 + 2) > (buf + len)) {
+	/* it's too short to even contain a payload length. skip it */
+	ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x too short (no payload length).\n", msg->id>>8, msg->id&0xFF);
 		    
-		} else if ((p + 2 + 2 + msg->length + 2) > (buf + len)) {
-		    /* it's long enough for a payload length, but the message is incomplete  */
-		    ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x incomplete.\n", msg->id>>8, msg->id&0xFF);
+    } else if ((p + 2 + 2 + msg->length + 2) > (buf + len)) {
+	/* it's long enough for a payload length, but the message is incomplete  */
+	ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x incomplete.\n", msg->id>>8, msg->id&0xFF);
 		    
-		} else if (msg->checksum != gps_msg_checksum(p+2, 2+2+msg->length)) {
-		    /* it looks like a NAV-PVT but there's a checksum error */
-		    ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x checksum error: msg=0x%04x calc=0x%04x.\n",
-			     msg->id>>8, msg->id&0xFF, msg->checksum, gps_msg_checksum(p+2, 2+2+msg->length));
-		    ESP_LOG_BUFFER_HEXDUMP(TAG, p, 2+2+2+msg->length+2, ESP_LOG_INFO);
+    } else if (msg->checksum != gps_msg_checksum(p+2, 2+2+msg->length)) {
+	/* it looks like a NAV-PVT but there's a checksum error */
+	ESP_LOGW(TAG, "Message id: 0x%02x 0x%02x checksum error: msg=0x%04x calc=0x%04x.\n",
+		 msg->id>>8, msg->id&0xFF, msg->checksum, gps_msg_checksum(p+2, 2+2+msg->length));
 		    
-		} else {
-		    /* we have a NAV-PVT, the only message we need, and it's format is valid (saying nothing about the GPS data validity) */
-		    ESP_LOGI(TAG,
-			     "NAV-PVT: %02d/%02d/%02d %02d:%02d:%02d, fix: %d, fixType: %d, numSV: %d, lat, lon: %d.%07d,%d.%07d checksum: 0x%04x", 
-			     msg->payload.navPVT.year, 
-			     msg->payload.navPVT.month, 
-			     msg->payload.navPVT.day,
-			     msg->payload.navPVT.hour, 
-			     msg->payload.navPVT.min, 
-			     msg->payload.navPVT.sec,
-			     msg->payload.navPVT.flags & 0x01,
-			     msg->payload.navPVT.fixType,
-			     msg->payload.navPVT.numSV,
-			     (msg->payload.navPVT.lat) / 10000000, abs((msg->payload.navPVT.lat) % 10000000),
-			     (msg->payload.navPVT.lon) / 10000000, abs((msg->payload.navPVT.lon) % 10000000),
-			     msg->checksum);
-		    /* check that BOTH the validDate and validTime flags are turned on */
-		    if ((msg->payload.navPVT.valid & NAV_PVT_VALID_DATE) && (msg->payload.navPVT.valid & NAV_PVT_VALID_TIME)) {
-			/* the first few messages sometimes have spurious data including date/time */
-			gps_write_gps_msg_to_file(msg);
-			fix = (msg->payload.navPVT.flags & 0x01);
+    } else {
+	/* we have a NAV-PVT, the only message we need, and its format is valid (saying nothing about the GPS data validity) */
+	ESP_LOGI(TAG,
+		 "NAV-PVT: %02d/%02d/%02d %02d:%02d:%02d, fix: %d, fixType: %d, numSV: %d, lat, lon: %d.%07d,%d.%07d checksum: 0x%04x", 
+		 msg->payload.navPVT.year, 
+		 msg->payload.navPVT.month, 
+		 msg->payload.navPVT.day,
+		 msg->payload.navPVT.hour, 
+		 msg->payload.navPVT.min, 
+		 msg->payload.navPVT.sec,
+		 msg->payload.navPVT.flags & 0x01,
+		 msg->payload.navPVT.fixType,
+		 msg->payload.navPVT.numSV,
+		 (msg->payload.navPVT.lat) / 10000000, abs((msg->payload.navPVT.lat) % 10000000),
+		 (msg->payload.navPVT.lon) / 10000000, abs((msg->payload.navPVT.lon) % 10000000),
+		 msg->checksum);
+	/* check that BOTH the validDate and validTime flags are turned on */
+	if ((msg->payload.navPVT.valid & NAV_PVT_VALID_DATE) && (msg->payload.navPVT.valid & NAV_PVT_VALID_TIME)) {
+	    /* the first few messages sometimes have spurious data including date/time */
+	    gps_write_gps_msg_to_file(msg);
+	    fix = (msg->payload.navPVT.flags & 0x01);
 			   
-		    } else {
-			ESP_LOGW(TAG, "Invalid date or time flag set: 0x%02x.\n",
-				 msg->payload.navPVT.valid);
-		    }
-		}
-		
-		/* blink slow if no fix, fast if fix */
-		if (fix) {
-		    gps_blink(10, 10, 1);
-		    fix = false; /* reset in case the next message is invalid */
-		} else {
-		    gps_blink(500, 500, 1);
-		}
-		
-		/* skip to next message in buffer (if there is one) */
-		p += 2 + 2 + 2 + msg->length + 2; /* signature + id + length + payload-length + checksum  */
-		
-		
-	    }
+	} else {
+	    ESP_LOGW(TAG, "Invalid date or time flag set: 0x%02x.\n",
+		     msg->payload.navPVT.valid);
 	}
     }
+		
+    /* blink red if no fix, green if fix */
+    if (fix) {
+	gps_blink(GPS_BLINK_GREEN, 10, 10, 1);
+	fix = false; /* reset in case the next message is invalid */
+    } else {
+	gps_blink(GPS_BLINK_RED, 10, 10, 1);
+    }
+}
+
+static void gps_handle_nmea_message(uint8_t *buf, size_t len) {
+    ESP_LOGI(TAG, "gps_handle_nmea_message: %d", len);
+    ESP_LOG_BUFFER_HEXDUMP(TAG, buf, len, ESP_LOG_INFO);
+    const char *resetMsg = "$GNTXT,01,01,02,u-blox AG - www.u-blox.com";
+
+    /* we are not supposed to get NMEA messages, it looks like the device has reset, re-initialise */
+    if (memcmp(buf, resetMsg, strlen(resetMsg)) == 0) {
+	ESP_LOGI(TAG, "re-sending UBX init string");
+	gps_init();
+    }
+}
+
+static void gps_handle_message(uint8_t *buf, size_t len) {
+    /* let's make the simplifying assumption that messages won't span multiple UART events. */
+    /* it may not always be true, but empirically it seems to be the case. */
+
     
+    uint8_t *p = buf;
+    size_t msglen;
     
-    
+    /* consider the possibility that this could be either a UBX or NMEA message */
+    while ((p != NULL) && (p < (buf + len))) {
+
+	/* search for 0xb5 0x62 as the signature for the start of a UBX message */
+	if ((*p == 0xb5) && (*(p+1) == 0x62)) {
+	    /* skip two bytes containing class and id, then read the two-byte length */
+	    msglen = 2 + 2 + *(uint16_t *)(p+4) + 2;  /* signature + id + length + payload-length + checksum  */
+	    gps_handle_ubx_message(p, msglen);
+	    
+	    /* skip to next message in buffer (if there is one) */
+	    p += msglen; 
+	    
+	} else if (*p == '$' || *p == '!') { /* these are the two valid NMEA start delimiters */
+	    /* find the end delimiter, which is CR LF */
+	    for (msglen = 0; (msglen < (len-(p-buf))); msglen++) {
+		if (*(p+msglen) == 0x0d && *(p+msglen+1) == 0x0a) {
+		    /* we found the terminal delimiter */
+		    msglen += 2; 	/* because it's a two character delimiter */
+		    gps_handle_nmea_message(p, msglen);
+		    break;
+		}
+	    }
+	    
+	    /* skip to next message in buffer (if there is one) */
+	    p += msglen; 
+	    
+	    
+	} else {
+	    /* either we got garbage, or we have not found the start of the message */
+	    p++;		/* skip a character and start again */
+	}
+    }
 }
 
 static void gps_event_task(void *pvParameters)
@@ -287,56 +377,6 @@ static void gps_event_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-void gps_init() {
-    // const char UBLOX_BAUD_RATE[] = {
-    // 0xB5,0x62,0x06,0x00,0x14,0x00,0x01,0x00,0x00,0x00,0x10,0x00,0x00,0x00,0x00,0xC2,0x01,0x00,0x01,0x00,0x01,0x00,0x00,
-    // 0x00,0x00,0x00,0xF0,0xCA // 115200 baud
-    // };
-    const char UBLOX_INIT[] = {
-	// Disable NMEA
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x24, // GxGGA off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x2B, // GxGLL off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x32, // GxGSA off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x39, // GxGSV off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x04,0x00,0x00,0x00,0x00,0x00,0x01,0x04,0x40, // GxRMC off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x05,0x00,0x00,0x00,0x00,0x00,0x01,0x05,0x47, // GxVTG off
-
-	// Disable UBX
-	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x07,0x00,0x00,0x00,0x00,0x00,0x00,0x17,0xDC, //NAV-PVT off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x12,0xB9, //NAV-POSLLH off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x13,0xC0, //NAV-STATUS off
-	0xB5,0x62,0x06,0x01,0x08,0x00,0x05,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x15,0xD2, //ACK-ACK off
-
-	// Enable UBX
-	0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x07,0x00,0x01,0x00,0x00,0x00,0x00,0x18,0xE1, //NAV-PVT on
-	// 0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x01,0x00,0x00,0x00,0x00,0x13,0xBE, //NAV-POSLLH on
-	// 0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x14,0xC5, //NAV-STATUS on
-
-	// Rate
-	// 0xB5,0x62,0x06,0x08,0x06,0x00,0x64,0x00,0x01,0x00,0x01,0x00,0x7A,0x12, //(10Hz)
-	// 0xB5,0x62,0x06,0x08,0x06,0x00,0xC8,0x00,0x01,0x00,0x01,0x00,0xDE,0x6A, //(5Hz)
-	// 0xB5,0x62,0x06,0x08,0x06,0x00,0xE8,0x03,0x01,0x00,0x01,0x00,0x01,0x39, //(1Hz)
-	0xB5,0x62,0x06,0x08,0x06,0x00,0x88,0x13,0x01,0x00,0x01,0x00,0xB1,0x49, //(0.2hz)
-
-	// GNSS type configuration
-	0xB5,0x62,0x06,0x3E,0x24,0x00,0x00,0x00,0x16,0x04,0x00,0x04,0xFF,0x00,0x01,0x00,0x00,0x01,0x01,0x01,0x03,0x00,0x01,
-	0x00,0x00,0x01,0x05,0x00,0x03,0x00,0x00,0x00,0x00,0x01,0x06,0x08,0xFF,0x00,0x00,0x00,0x00,0x01,0xA5,0x39,
-
-	// Power Save mode
-	0xB5,0x62,0x06,0x11,0x02,0x00,0x08,0x01,0x22,0x92
-    };
-
-    // uart_write_bytes(UART_NUM, UBLOX_BAUD_RATE, sizeof UBLOX_BAUD_RATE);
-    // ESP_ERROR_CHECK(uart_flush(UART_NUM));
-    // vTaskDelay(1000 / portTICK_RATE_MS); /* allow time for acks to come in */
-    // ESP_ERROR_CHECK(uart_set_baudrate(UART_NUM, 115200));
-    uint32_t baud_rate;
-    uart_get_baudrate(UART_NUM, &baud_rate);
-    ESP_LOGI(TAG, "baud rate: %d", baud_rate);
-    uart_write_bytes(UART_NUM, UBLOX_INIT, sizeof UBLOX_INIT);
-    return;
-}
-
 void gps_main()
 {
     esp_log_level_set(TAG, ESP_LOG_VERBOSE);
@@ -360,9 +400,11 @@ void gps_main()
     uart_driver_install(UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
     gps_init();		/* specify what messages we want / don't want */
 
-    // Setup a GPIO to blink LED when a valid GPS reading is received
-    gpio_pad_select_gpio(GPS_BLINK_LED);
-    ESP_ERROR_CHECK(gpio_set_direction(GPS_BLINK_LED, GPIO_MODE_OUTPUT));
+    // Setup a GPIO to blink LED when a valid/invaid GPS reading is received
+    gpio_pad_select_gpio(GPS_BLINK_GREEN);
+    ESP_ERROR_CHECK(gpio_set_direction(GPS_BLINK_GREEN, GPIO_MODE_OUTPUT));
+    gpio_pad_select_gpio(GPS_BLINK_RED);
+    ESP_ERROR_CHECK(gpio_set_direction(GPS_BLINK_RED, GPIO_MODE_OUTPUT));
 
     //Create a task to handler UART event from ISR
     xTaskCreate(gps_event_task, "gps_event_task", 2048, NULL, 12, NULL);
